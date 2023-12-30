8af339664e2096dac1746c81af4fd33971f99dac
oleg.oleg-ivanov2014@yandex.ru <net-qwerty>
2023-12-30 11:56:51 +0300

    Add method sendForm, sending data in API

--- demographic_data/FormItem.cs
+++ demographic_data/FormItem.cs
@@ -1,12 +1,23 @@
-// using Microsoft.AspNetCore.Authorization.Infrastructure;
-// using System;
-// using System.Reflection;
-// public class FormItem
-// {
-//     public string? Title { get; set; }
-//     public bool IsDone { get; set; } = false;
-//     public string Name = "TESTname";
-//     Dictionary<string, string> Field = new Dictionary<string, string>();
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
+public class FormItem
+{
+    public int Id { get; set; }
 
-// }
+    public string? Birthday { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? PlaceOfBirth { get; set; }
+
+    public string? Education { get; set; }
+
+    public string? FamilyStatus { get; set; }
+
+    public string? Employment { get; set; }
+
+    public string? IncomeLevel { get; set; }
+
+    public string? Revenue { get; set; }
+
+}
--- demographic_data/Program.cs
+++ demographic_data/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using demographic_data;
 using System;
 using System.Reflection;
@@ -8,6 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://127.0.0.1:5201/") });
 
 await builder.Build().RunAsync();
