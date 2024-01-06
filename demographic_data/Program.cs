71e23c6343dc012c1b55666296a85264d1cef814
oleg.oleg-ivanov2014@yandex.ru <net-qwerty>
2024-01-06 19:06:09 +0300

    add page Analitics.razor

--- demographic_data/Program.cs
+++ demographic_data/Program.cs
@@ -4,11 +4,14 @@ using Microsoft.Extensions.Configuration;
 using demographic_data;
 using System;
 using System.Reflection;
+using Microsoft.AspNetCore.Components.QuickGrid;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://127.0.0.1:5201/") });
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
 
 await builder.Build().RunAsync();
